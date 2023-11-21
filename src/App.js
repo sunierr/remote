8d0285e1c984a4f2e945f417c5d28261d5d422fc
sunier <sonor.fz@163.com>
2023-11-21 14:25:55 +0800

    added up and down button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronUp, faChevronDown, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+
 import './App.css';
+
+//icon({name: 'user'});
 
 function App() {
   async function handleClick(e) {
     console.log('working')
     try {
-      await fetch('http://10.60.25.24:3001/'+e.target.value, {
-        method: "GET", 
+      await fetch('http://10.60.25.24:3001/' + e.target.value, {
+        method: "GET",
         mode: "no-cors"
       });
       console.log("request sent");
     } catch (error) {
-      console.error("Error:", error); 
+      console.error("Error:", error);
     }
   };
   return (
     <div className="App">
-     <button className='button-30' value="0" onClick={handleClick}>左</button>
-     <button className='button-30' value="1" onClick={handleClick}>右</button>
-     <button className='button-30' value="2" onClick={handleClick}>确认</button>
-     <button className='button-30' value="3" onClick={handleClick}>关闭</button>
+      <button className='button-30' value="0" onClick={handleClick}><FontAwesomeIcon icon={faChevronLeft} /></button>
+      <button className='button-30' value="1" onClick={handleClick}><FontAwesomeIcon icon={faChevronRight} /></button>
+      <button className='button-30' value="4" onClick={handleClick}><FontAwesomeIcon icon={faChevronUp} /></button>
+      <button className='button-30' value="5" onClick={handleClick}><FontAwesomeIcon icon={faChevronDown} /></button>
+      <button className='button-30' value="2" onClick={handleClick}>确认</button>
+      <button className='button-30' value="3" onClick={handleClick}>关闭</button>
     </div>
   );
 }
